4ea6f048a43248b811277e8caf1fb3aef3955521
Yang Jinwook <yang@proszet.com>
2024-03-18 14:50:42 +0900

    Add restriction editting user profile bypassing with url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, except: [:show, :index, :new]
+  before_action :require_same_user, only: [:edit, :update]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -46,4 +48,12 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
+  def require_same_user
+    if current_user != @user
+      flash[:elert] = "You can only edit or delete your own article"
+      redirect_to @user
+    end
+  end
+
 end
